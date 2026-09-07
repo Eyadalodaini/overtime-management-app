348ed4141a463e66fb941d91007897f12feffa06
Eyadalodaini <eaadalodaini@gmail.com>
2026-09-07 12:07:33 +0300

    Update route.ts

--- src/app/api/records/route.ts
+++ src/app/api/records/route.ts
@@ -1,4 +1,4 @@
-export const dynamic = 'force-static';
+export const dynamic = "force-static";
 import { NextResponse } from "next/server";
 import { getRecords, createRecord } from "@/lib/db-access";
 import { calculateAmount } from "@/lib/utils";
